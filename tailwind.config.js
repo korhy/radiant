c92c2e4067b6937ba59716e13813cb64610244f0
Clément Boudinel <clementboudinel@gmail.com>
2024-10-23 19:09:55 +0200

    Implement Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'bg-blue-950' : '#23313e'
+      }
+    },
+  },
+  plugins: [],
+}
